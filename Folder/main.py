dbe895f8676bdc6f4ec3440d1db6f50e9d0b4d05
SieurFenouil <cedryc.fenot@gmail.com>
2025-03-13 21:57:44 -0500

    Todo list
    more detailed list of future ideas

--- Folder/main.py
+++ Folder/main.py
@@ -6,10 +6,25 @@ Created on Sat Mar  1 19:44:29 2025
 """
 
 #TODO add plot / histogram of who wins
-#TODO add voters willingness to participate
-#TODO add voters set_favorite_party
-#TODO have other parties adapt their ideologies ?
-#TODO add political campaigns
+
+#TODO do ideology as multidimentional tuple
+
+#TODO parties
+# - add popularity attribute
+# - change popularity based on number of votes and donations
+# - extra random popularity hit for election winners. ( scales with ideology ?)
+# - ideological changes to match voter average
+# - ideological changes to match base average
+# - willingness to change ideology ( scales with ideology ?)
+
+#TODO voters
+# - willingness to participate. ( scales with ideology ?)
+# - willingness to donate. ( scales with ideology ?)
+# - voters get closer to one another
+# - voters get attracted to parties (see gravity and planets)
+# - random big drifts every few elections
+# - willingness to change ideology ( scales with ideology ?)
+
 
 
 from simulation_environment import Simulation
