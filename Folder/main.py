85624b3e14e3663defb6950fd15e6f7cc695de4b
SieurFenouil <cedryc.fenot@gmail.com>
2025-03-13 21:40:11 -0500

    New :
    - Repeatable simulation with input prompts
    - Selectable number of years, with new random vote list each year

--- Folder/main.py
+++ Folder/main.py
@@ -5,7 +5,6 @@ Created on Sat Mar  1 19:44:29 2025
 @author: User
 """
 
-#TODO pick election winners
 #TODO add plot / histogram of who wins
 #TODO add voters willingness to participate
 #TODO add voters set_favorite_party
@@ -15,15 +14,21 @@ Created on Sat Mar  1 19:44:29 2025
 
 from simulation_environment import Simulation
 
-
+def launch_simulation():
+    
+    elections_number = int(input( "how many elections ?"))
+    parties_number = int(input( "how many parties ?"))
+    voters_number = int(input( "how many voters ?"))
             
-        
-        
-Sim = Simulation()
-
-Sim.generate_party_list(5)
-Sim.generate_voter_list(500)
-Sim.election_day()
+    Sim = Simulation()
+    
+    Sim.generate_party_list(parties_number)
+    
+    for i in range(elections_number):
+    
+        print( "----- Year ", i+1, " -----")
+        Sim.generate_voter_list(voters_number)
+        Sim.election_day()
 
 
 
